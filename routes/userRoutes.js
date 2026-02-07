853b3da5d85b8d1a611f8451a271ce4871df5f85
Nua <nurayat1811@gmail.com>
2026-02-07 23:42:05 +0500

    Fix validate middleware import

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 const { getProfile, updateProfile } = require("../controllers/userController");
+const { validate } = require("../middleware/validate");
 
 router.get("/profile", auth, getProfile);
 router.put("/profile", auth, validate(updateProfileSchema), updateProfile);
