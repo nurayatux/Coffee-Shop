33fa2a9599a81adbc643e8ce17cfc3ef789ae299
Nua <nurayat1811@gmail.com>
2026-02-07 23:53:31 +0500

    Fix schemas require path

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const auth = require("../middleware/authMiddleware");
 const { getProfile, updateProfile } = require("../controllers/userController");
 const { validate } = require("../middleware/validate");
-const { updateProfileSchema } = require("../schemas");
+const { updateProfileSchema } = require("../middleware/schemas");
 
 router.get("/profile", auth, getProfile);
 router.put("/profile", auth, validate(updateProfileSchema), updateProfile);
