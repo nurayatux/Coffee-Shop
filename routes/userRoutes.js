e4fc71a38301d06dcad0f956fb4ce69ff8321cd2
Nua <nurayat1811@gmail.com>
2026-02-07 23:49:54 +0500

    Import updateProfileSchema in user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,11 @@
 
 const express = require("express");
 const router = express.Router();
+
 const auth = require("../middleware/authMiddleware");
 const { getProfile, updateProfile } = require("../controllers/userController");
 const { validate } = require("../middleware/validate");
+const { updateProfileSchema } = require("../schemas");
 
 router.get("/profile", auth, getProfile);
 router.put("/profile", auth, validate(updateProfileSchema), updateProfile);
